ffa7c92e2eb77a5d1a9185898fadd95f8ba1dbfe
harindu-pe <harindu.pe@gmail.com>
2025-01-25 19:38:31 +0530

    exercise 3.4 done

--- index.js
+++ index.js
@@ -43,6 +43,13 @@ app.get("/api/persons/:id", (req, res) => {
   }
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
+  console.log(persons);
+});
+
 app.get("/info", (req, res) => {
   const serverTime = new Date();
   const html = `Phonebook has info for ${persons.length} people <br/><br/> ${serverTime}`;
